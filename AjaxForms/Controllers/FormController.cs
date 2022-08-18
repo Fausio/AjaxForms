1725697d9239407a5fbb1daaa1dab7339ef16312
fausio luis <fausioluis@live.com>
2022-08-18 22:48:52 +0200

    controler

---
+++ AjaxForms/Controllers/FormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxForms.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult form()
+        {
+            return View();
+        }
+    }
+}
