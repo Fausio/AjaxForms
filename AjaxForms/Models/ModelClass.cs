7d46aa0516b1b0d1ea24fe904019c18f02675195
fausio luis <fausioluis@live.com>
2022-08-18 23:41:33 +0200

    encryptString

---
+++ AjaxForms/Models/ModelClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace AjaxForms.Models
+{
+     
+
+    public class ModelClass
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+
+        public string encryptString(string value)
+        {
+            byte[] b = Encoding.ASCII.GetBytes(value);
+            string envalue = Convert.ToBase64String(b);
+            return envalue;
+        }
+    }
+}
