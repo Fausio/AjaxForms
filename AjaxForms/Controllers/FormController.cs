c9fefd58f5e324d4c14a81fd62d6b4244829224d
fausio luis <fausioluis@live.com>
2022-08-19 00:19:51 +0200

    formsubmitajax and use json app connection

--- AjaxForms/Controllers/FormController.cs
+++ AjaxForms/Controllers/FormController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using AjaxForms.Models;
 
 namespace AjaxForms.Controllers
 {
     public class FormController : Controller
     {
+        IConfiguration configuration;
+
+        public FormController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public IActionResult form()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult formsubmitajax(string name, string username, string password)
+        {
+             List<string> strings = new List<string>();
+            SqlConnection connection = new SqlConnection(configuration.GetConnectionString("SQLServerConnection"));
+
+            var query = $@"INSERT INTO [TABLE]([Name],[UserName],[Password])  VALUES ({name},{username},{ModelClass.encryptString(password)})";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.ExecuteNonQuery();
+            strings.Add("Tuple Inserted.");
+            return Json(strings);
         }
     }
 }
