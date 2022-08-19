92dd5b291ee41dc7224a7dd8b8cb4c49ef9d9c64
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-08-19 09:15:33 +0200

    model ModelObject

--- AjaxForms/Models/ModelClass.cs
+++ AjaxForms/Models/ModelClass.cs
@@ -7,13 +7,19 @@ namespace AjaxForms.Models
 
     public static class ModelClass
     {
-  
-
         public static  string encryptString(this string value)
         {
             byte[] b = Encoding.ASCII.GetBytes(value);
             string envalue = Convert.ToBase64String(b);
             return envalue;
         }
+    }
+
+
+    public class ModelObject
+    {
+        public string Name { get; set; }
+         public string UserName { get; set; }
+         public string Password { get; set; }
     }
 }
